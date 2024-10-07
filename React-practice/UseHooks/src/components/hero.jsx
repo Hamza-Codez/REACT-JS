0ef68d1914e51f17f3edb01910bf385f2265f848
HamzaThepixelpro <ha444777ha@gmail.com>
2024-10-07 19:39:23 +0500

    hero section_hit&run

--- React-practice/UseHooks/src/components/hero.jsx
+++ React-practice/UseHooks/src/components/hero.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='relative p-8 bg-deepBlue'>
+    <section className='relative p-8 bg-deepBlue text-white'>
 
-      <div className='flex '>
-        <div>
-          <h1 className='text-white'>Power Your Finance Grow Your Business</h1>
+      <div className='flex flex-row mx-8 gap-9 items-center'>
+        <div className='space-y-8 w-[40%]'>
+          <h1 className='text-white font-mulish font-bold text-[40px] leading-[1.2]'>Power Your Finance Grow Your Business</h1>
           <div className='w-6 h-1 bg-greenLight'>
           </div>
-          <p >
+          <p className='text-white font'>
             Accept Payments from customers, Automate payouts to vendors & employees. Never run out of working capital.
           </p>
-          <button>  
+          <button className='bg-lightBlue px-2 py-1 rounded-md'>  
             Sign Up Now
           </button>
         </div>
-        <img src="/images/hero-illustration.jpg"/>
+        <img className='w-[50%]' src="/images/hero-illustration.jpg"/>
       </div>
       <div>
         <img src="./images/hero-shape.svg" alt="triangle" />
