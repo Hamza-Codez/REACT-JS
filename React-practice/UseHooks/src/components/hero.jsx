61e8072ceccc9a002fa8dc8fd0ecfe3cee9103fd
HamzaThepixelpro <ha444777ha@gmail.com>
2024-10-09 00:20:53 +0500

    done with razorpay hero section

--- React-practice/UseHooks/src/components/hero.jsx
+++ React-practice/UseHooks/src/components/hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='relative text-white'>
+    <section className='relative bg bg-deepBlue'>
 
-      <div className='flex flex-row items-center gap-5 bg-deepBlue pl-12 pr-8'>
+      <div className='flex flex-row items-center gap-5 pl-12 pr-8 mx-auto justify-between text-white w-full  max-w-[1300px]'>
         <div className='space-y-8 w-[32rem]'>
           <h1 className='text-white font-mulish font-bold text-[45px] leading-[1.2]'>Power Your Finance, Grow Your Business</h1>
           <div className='w-6 h-1 bg-greenLight'>
@@ -12,14 +12,14 @@ const Hero = () => {
           <p className='text-white font text-[18px] opacity-65'>
             Accept Payments from customers, Automate payouts to vendors & employees. Never run out of working capital.
           </p>
-          <button className='bg-lightBlue px-2 py-2 rounded-sm'>  
+          <button className='bg-lightBlue px-3 py-2 rounded-md hover:bg-lightBlue500 transition-all duration-500 font-semibold text-[1rem]'>  
             Sign Up Now
           </button>
         </div>
-        <img className='w-[56%]' src="/images/hero-illustration.jpg"/>
+        <img className='w-full max-w-[680px]' src="/images/hero-illustration.jpg"/>
       </div>
-      <div className='mt-[-1px] mr-[-1px]'>
-        <img className='min-w-full min-h-[12rem]' src="./images/hero-shape.svg" alt="triangle" />
+      <div className='absolute right-0 left-0 -mt-1'>
+        <img className=' min-w-[100.1%] ' src="./images/hero-shape.svg"/>
       </div>
     </section>
   )
