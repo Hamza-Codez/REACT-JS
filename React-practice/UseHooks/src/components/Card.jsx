311b888191b3f99d6c326b60dfa5afc3e73abe25
HamzaThepixelpro <ha444777ha@gmail.com>
2024-10-11 23:49:55 +0500

    Crads Section>>>

---
+++ React-practice/UseHooks/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h2></h2>
+          <img src=""/>
+        </div>
+        <p>
+
+        </p>
+        <a>
+          
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Card
