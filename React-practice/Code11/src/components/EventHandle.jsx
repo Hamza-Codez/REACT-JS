e338880776048e17ca9f9e3ab196cbb774b802c6
HamzaThepixelpro <ha444777ha@gmail.com>
2024-10-18 23:58:33 +0500

    Event Handling part2

--- React-practice/Code11/src/components/EventHandle.jsx
+++ React-practice/Code11/src/components/EventHandle.jsx
@@ -5,17 +5,25 @@ const EventHandle = () => {
     alert("I am clicked!")
   }
   function clicked(){
-    alert('hoverd')
+    alert('hovered')
+  }
+  function submited(){
+    alert('Are you sure About Submitting Your Form')
   }
   return (
     <>
-      <p onMouseOver={clicked()}>
+      {/* <p onMouseOver={clicked}>
         I am the Paragrah.
-      </p>
+      </p> */}
       <h1>HAMZA Ahmad</h1>
       <button onClick={run} className='bg-slate-100 text-black p-1 rounded-sm'>
         click me 
       </button>
+      <form onSubmit={submited}>
+        <input type="text" />
+        <button className='bg-slate-100 text-black p-1 rounded-sm' type='submit'>Submit</button>
+      </form>
+        <button className='bg-slate-100 text-black p-1 rounded-sm' onClick={() => alert('Hello World!')}>Click me</button>
     </>
   )
 }
